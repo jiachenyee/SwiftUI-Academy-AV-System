89fd47e5fc08f7634d9dd053c210e3c9d1c8cba8
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2023-09-05 00:29:10 +0800

    Initial Commit

---
+++ SwiftUI-Academy-AV-System/SwiftUI_Academy_AV_SystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Academy_AV_SystemApp.swift
+//  SwiftUI-Academy-AV-System
+//
+//  Created by Jia Chen Yee on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Academy_AV_SystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
